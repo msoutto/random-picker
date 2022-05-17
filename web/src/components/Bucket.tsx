746208e95b533374dd04a12eaaad0ba38e2cf368
Matheus Soutto <matheussoutto27@gmail.com>
2022-05-17 09:02:47 -0300

    styling Bucket

--- web/src/components/Bucket.tsx
+++ web/src/components/Bucket.tsx
@@ -3,13 +3,15 @@ import { useState } from "react";
 export function Bucket() {
   const items = ["Teste 1", "Teste 2", "Teste 3"];
   const listItems = items.map((item) =>
-    <li key={item}>
+    <li key={item} className="rounded border border-zinc-400">
       {item}
     </li>
   );
   return (
-    <div className="flow-root">
-      <ul>{listItems}</ul>
+    <div className="m-8">
+      <ul className="flex flex-col gap-2">
+        {listItems}
+      </ul>
     </div>
     
   );
