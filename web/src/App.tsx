dc64ca1e49110581b3f8fe67ebc35163930dadad
Matheus Soutto <matheussoutto27@gmail.com>
2022-06-03 13:38:40 +0000

    PickRandomButton working

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -5,17 +5,25 @@ import { PickRandomButton } from './components/PickRandomButton';
 
 export function App() {
   const [items, setItems] = useState(['Teste 1', 'Teste 2', 'Teste 3']);
+  const [pickedItem, setPickedItem] = useState<number>();
 
   const addToBucket = (item: string) => {
     setItems([...items, item]);
   };
+
+  const pickItem = (index: number) => {
+    if (index >= 0) {
+      setPickedItem(index);
+      console.log(index, items[index]);
+    }
+  }
 
   return (
     <div className="flex justify-center w-[100%] mt-16">
       <div className="flex flex-col w-[75%]">
         <AddForm addToBucket={addToBucket} />
         <Bucket items={items} />
-        <PickRandomButton items={items} />
+        <PickRandomButton items={items} pickItem={pickItem} />
       </div>
     </div>
   );
--- web/src/components/PickRandomButton.tsx
+++ web/src/components/PickRandomButton.tsx
@@ -1,10 +1,17 @@
 interface PickRandomButtonProps {
     items: Array<string>;
+    pickItem: (index: number) => void;
 }
 
-export function PickRandomButton({ items }: PickRandomButtonProps) {
+export function PickRandomButton({ items, pickItem }: PickRandomButtonProps) {
     const pickRandom = () => {
+        if (items.length < 1)
+            pickItem(-1);
         
+        // random from 0 to items.length
+        let index = Math.floor(Math.random() * items.length);
+        console.log(index);
+        pickItem(index);
     }
 
     return (
