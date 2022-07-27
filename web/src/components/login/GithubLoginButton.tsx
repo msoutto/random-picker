c7ba42c722453c2db9d1b846ab49e9350b6ad186
Matheus Soutto <matheussoutto27@gmail.com>
2022-07-26 22:25:33 -0300

    Creating GitHubLoginButton component

---
+++ web/src/components/login/GithubLoginButton.tsx
@@ -0,0 +1,27 @@
+interface GithubLoginButtonProps {
+    authorizeUrl: string;
+    clientId: string;
+    pickItem: (index: number) => void
+}
+
+export function GithubLoginButton({ authorizeUrl, clientId }: GithubLoginButtonProps) {
+    const callAuthorize = async () => {
+        const response = await fetch(`${authorizeUrl}?client_id=${clientId}`, {
+            method: 'GET',
+            headers: { 'Content-Type': 'application/json' }
+        });
+
+        console.log(response);
+
+        return await response.json();
+    }
+
+    return (
+        <div className="flex justify-center items-center mx-4">
+            <button 
+                className="button w-full"
+                onClick={callAuthorize}
+            >Login with Github</button>
+        </div>
+    );
+}
